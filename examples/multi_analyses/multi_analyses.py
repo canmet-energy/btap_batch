4a75e7c03e1784c37d7717345ce9b4f3a8b890ae
Lopez <phylroy.lopez@NRCan-RNCan.gc.ca>
2021-03-23 20:23:01 -0400

    modified multi run to use threads

--- examples/multi_analyses/multi_analyses.py
+++ examples/multi_analyses/multi_analyses.py
@@ -4,13 +4,15 @@ import logging
 import yaml
 from datetime import date
 import re
-
+import itertools
+import multiprocessing
+import concurrent.futures
+#
 # Displays logging.. Set to INFO or DEBUG for a more verbose output.
 logging.basicConfig(level=logging.ERROR)
 OPTIONS_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'options.yml')
 building_types =  [
-      'MidriseApartment',
-      'HighriseApartment',
+      'FullServiceRestaurant',
   ]
 
 epw_files = [
@@ -31,7 +33,7 @@ primary_heating_fuels =  [
 # Optimization data block.
 optimization = {
                 ":type": "nsga2",
-                ":population": 12,
+                ":population": 2,
                 ":n_generations": 1,
                 ":prob": 0.85,
                 ":eta": 3.0,
@@ -40,28 +42,49 @@ optimization = {
                     "cost_equipment_total_cost_per_m_sq"]
 }
 
-for epw_file in epw_files:
-    short_city_name = re.compile("(.*?)\.").search(epw_file)[1]
-    for building_type in building_types:
-        for primary_heating_fuel in primary_heating_fuels:
-            # Open the yaml in analysis dict.
-            with open(OPTIONS_FILE, 'r') as stream:
-                analysis = yaml.safe_load(stream)
-            analysis[':analysis_configuration'][':algorithm'] = optimization
-            analysis[':analysis_configuration'][':analysis_name'] = f"{short_city_name}_{building_type}_{primary_heating_fuel}"
-            analysis[':analysis_configuration'][':kill_database'] = False
-            analysis[':building_options'][':epw_file'] = [epw_file]
-            analysis[':building_options'][':building_type'] = [building_type]
-            analysis[':building_options'][':primary_heating_fuel'] = [primary_heating_fuel]
-            print(analysis[':analysis_configuration'][':analysis_name'])
-            input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),f"{analysis[':analysis_configuration'][':analysis_name']}.yml")
-            with open(input_file, 'w') as outfile:
-                 yaml.dump(analysis, outfile)
-            analysis = btap.btap_batch( analysis_config_file=input_file, git_api_token=os.environ['GIT_API_TOKEN'])
-            analysis.run()
+
+def run_analysis(short_city_name,
+                                               building_type,
+                                               primary_heating_fuel,
+                                               epw_file):
+    # Open the yaml in analysis dict.
+    with open(OPTIONS_FILE, 'r') as stream:
+        analysis = yaml.safe_load(stream)
+    analysis[':analysis_configuration'][':algorithm'] = optimization
+    analysis[':analysis_configuration'][':analysis_name'] = f"{short_city_name}_{building_type}_{primary_heating_fuel}"
+    analysis[':analysis_configuration'][':kill_database'] = False
+    analysis[':building_options'][':epw_file'] = [epw_file]
+    analysis[':building_options'][':building_type'] = [building_type]
+    analysis[':building_options'][':primary_heating_fuel'] = [primary_heating_fuel]
+    print(analysis[':analysis_configuration'][':analysis_name'])
+    input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                              f"{analysis[':analysis_configuration'][':analysis_name']}.yml")
+    with open(input_file, 'w') as outfile:
+        yaml.dump(analysis, outfile)
+    analysis = btap.btap_batch(analysis_config_file=input_file, git_api_token=os.environ['GIT_API_TOKEN'])
+    analysis.run()
+
+#Create database
+database = btap.BTAPDatabase()
+with concurrent.futures.ThreadPoolExecutor(14) as executor:
+    futures = []
+    for epw_file in epw_files:
+        short_city_name = re.compile("(.*?)\.").search(epw_file)[1]
+        for building_type in building_types:
+            for primary_heating_fuel in primary_heating_fuels:
+                # Executes docker simulation in a thread
+                print(epw_file)
+                futures.append(executor.submit(run_analysis,
+                                               short_city_name=short_city_name,
+                                               building_type=building_type,
+                                               primary_heating_fuel=primary_heating_fuel,
+                                               epw_file=epw_file
+                                               ))
+    for future in concurrent.futures.as_completed(futures):
+        print(future)
 
 #output all data from database.
-database = btap.BTAPDatabase()
+
 output_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 database.generate_output_files( analysis_id = None, output_folder = output_folder)
 
