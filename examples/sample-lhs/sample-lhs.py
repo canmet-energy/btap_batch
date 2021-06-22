e6b0478ced64f75d105a8eac7f8f14c4bdda9790
plopez <phylroy.lopez@canada.ca>
2021-06-22 16:42:57 -0400

    updated lhs example for tomorrows work with StatsCan

--- examples/sample-lhs/sample-lhs.py
+++ examples/sample-lhs/sample-lhs.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.ERROR)
 git_api_token = os.environ['GIT_API_TOKEN']
 
 # Set input file path.
-input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),'sampling-lhs.yml')
+input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),'sample-lhs.yml')
 
 # Initialize the analysis object.
 bb = btap.btap_batch(
