93eed25a162b88aaa71aea1083302e151b5c89d4
Gilani <sara.gilani@NRCan-RNCan.gc.ca>
2021-07-27 15:20:07 -0400

    added ecm packages simulated for updated highrise (i.e. in-suite doas and updated space type of the office room) instead of optimization

---
+++ examples/parametric/highrise_elec.py
@@ -0,0 +1,24 @@
+import src.btap_batch as btap
+import os
+import logging
+
+# Displays logging.. Set to INFO or DEBUG for a more verbose output.
+logging.basicConfig(level=logging.ERROR)
+
+# Your git token.. Do not commit this!
+git_api_token = os.environ['GIT_API_TOKEN']
+
+# Set input file path.
+input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),'highrise_elec.yml')
+
+# Initialize the analysis object.
+bb = btap.btap_batch(
+    # Input file.
+    analysis_config_file=input_file,
+    git_api_token=git_api_token
+)
+
+# Run the analysis
+bb.run()
+
+
