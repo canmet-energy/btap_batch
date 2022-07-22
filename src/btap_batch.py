c6e9e193b948ccda0745e7ece8dfb48fd521e21d
Chris Kirney <chris.kirney@canada.ca>
2022-07-22 06:57:10 -0400

    Fixing typo when setting rebuild variable state.

--- src/btap_batch.py
+++ src/btap_batch.py
@@ -458,7 +458,7 @@ class AWSBatch:
     def build_image(self, rebuild=False):
         
         # Ensure image is rebuilt if requested
-        reubild=self.rebuild_image
+        rebuild=self.rebuild_image
 
         self.image_tag = self.credentials.user_name
         self.image_full_name = f'{self.credentials.account_id}.dkr.ecr.{self.credentials.region_name}.amazonaws.com/' + self.image_name + ':' + self.image_tag
