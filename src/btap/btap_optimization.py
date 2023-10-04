f62a5e64a8d6685e8105ed14606b3c036ed3954c
plopez <phylroy.lopez@gmail.com>
2023-10-04 16:55:40 +0000

    adding reports

--- src/btap/btap_optimization.py
+++ src/btap/btap_optimization.py
@@ -88,13 +88,13 @@ class BTAPOptimization(BTAPAnalysis):
                  analysis_config=None,
                  output_folder=None,
                  analysis_input_folder=None,
-                 reference_run_data_path=None
+                 reference_run_df=None
                  ):
         # Run super initializer to set up default variables.
         super().__init__(analysis_config=analysis_config,
                          output_folder=output_folder,
                          analysis_input_folder=analysis_input_folder,
-                         reference_run_data_path=reference_run_data_path
+                         reference_run_df=reference_run_df
                          )
         self.max_number_of_simulations = None
 
