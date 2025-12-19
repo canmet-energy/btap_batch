751e76809b21fb82c667d0d0471a354bf9f018c9
Chris Kirney <chris.kirney@canada.ca>
2025-12-19 07:31:59 -0500

    Try without delay

--- src/btap/btap_parametric.py
+++ src/btap/btap_parametric.py
@@ -117,8 +117,8 @@ class BTAPParametric(BTAPAnalysis):
                 # go through each option scenario
                 for run_options in self.scenarios:
                     # Executes docker simulation in a thread 
-                    if (self.file_number > 500) and (self.compute_environment == 'local_managed_aws_workers'):
-                        time.sleep(5+random())  # slight delay to avoid overwhelming system
+                    #if (self.file_number > 500) and (self.compute_environment == 'local_managed_aws_workers'):
+                    #    time.sleep(5+random())  # slight delay to avoid overwhelming system
                     futures.append(executor.submit(self.run_datapoint, run_options=run_options))
                 # Bring simulation thread back to main thread
                 for future in concurrent.futures.as_completed(futures):
